32f864da9a2255d4cbceeb2796f6b48c5a2dd5f2
Shanthan Cheruku <shanthanc@gmail.com>
2022-10-20 14:17:57 -0500

    my first commit

---
+++ src/test/java/com/shanthan/chicagodivvydata/ChicagoDivvyDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanthan.chicagodivvydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicagoDivvyDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
